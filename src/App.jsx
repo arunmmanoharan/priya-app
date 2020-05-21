b0aec99440594f9cf4d9206d2fcaf9153dadd05a
Arun Manohar Manoharan <amanoharan@trinitypartners.com>
2020-05-20 23:04:55 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import BootstrapTable from 'react-bootstrap-table-next';
+import './App.scss';
+
+const products = [{
+  id: 1,
+  jobName: 'mnv2_d_UND_rpt',
+  startTime: '05/20/2020 03:00:01',
+  endTime: '05/20/2020 03:00:02',
+  status: 'SU'
+}];
+const columns = [{
+  dataField: 'jobName',
+  text: 'Job Name',
+  sort: true
+}, {
+  dataField: 'startTime',
+  text: 'Start Time',
+  sort: true
+}, {
+  dataField: 'endTime',
+  text: 'End Time',
+  sort: true
+}, {
+  dataField: 'status',
+  text: 'Status',
+  sort: true
+}];
+
+const App = () => {
+  return (
+    <div className="App">
+      <BootstrapTable bootstrap4 keyField='id' data={ products } columns={ columns } />
+    </div>
+  );
+}
+
+export default App;
